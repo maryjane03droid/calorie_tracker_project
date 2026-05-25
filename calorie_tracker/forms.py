2cd920929550d701356d9fb52461689210e802ab
Maryjane <kuthiemaryjane@gmail.com>
2026-05-26 01:06:20 +0300

    Create food item form

---
+++ calorie_tracker/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import FoodItem
+
+
+class FoodItemForm(forms.ModelForm):
+    class Meta:
+        model = FoodItem
+        fields = ['name', 'quantity', 'calories']
